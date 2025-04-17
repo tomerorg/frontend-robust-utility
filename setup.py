d558dfb0c36a8926fd1318a1a040952911d0f892
tomerbr3456 <59730830+tomerbr3456@users.noreply.github.com>
2025-04-17 13:23:14 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="frontend-robust-utility",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.1",
+        "pandas>=1.2.0",
+        "numpy>=1.20.0",
+    ],
+    author="",
+    author_email="",
+    description="Python frontend utility with SQLAlchemy and Flask and Pandas",
+    keywords="frontend-robust-utility, python",
+    url="",
+)
